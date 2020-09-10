555ecf05c723348c965a5650cdf9488e02ac98d7
James Herdman <james.herdman@gmail.com>
2020-09-10 08:17:40 -0400

    Update Ember

--- addon/helpers/format-currency.js
+++ addon/helpers/format-currency.js
@@ -1,10 +1,6 @@
 import { helper as buildHelper } from '@ember/component/helper';
-import { default as formatCurrencySuper } from '@precision-nutrition/number-formatting/utils/format-currency';
+import formatCurrency from '@precision-nutrition/number-formatting/utils/format-currency';
 
-export function formatCurrency(_, { value }) {
-  let formatted = formatCurrencySuper(value);
-
-  return formatted;
-}
-
-export default buildHelper(formatCurrency);
+export default buildHelper(function(_, { value }) {
+  return formatCurrency(value);
+});
--- addon/helpers/pretty-percentage.js
+++ addon/helpers/pretty-percentage.js
@@ -1,10 +1,6 @@
 import { helper as buildHelper } from '@ember/component/helper';
-import { default as prettyPercentageSuper } from '@precision-nutrition/number-formatting/utils/pretty-percentage';
+import prettyPercentage from '@precision-nutrition/number-formatting/utils/pretty-percentage';
 
-export function prettyPercentage(_, { decimalValue, precision=0 }) {
-  let value = prettyPercentageSuper(decimalValue, precision);
-
-  return value;
-}
-
-export default buildHelper(prettyPercentage);
+export default buildHelper(function(_, { decimalValue, precision=0 }) {
+  return prettyPercentage(decimalValue, precision);
+});
